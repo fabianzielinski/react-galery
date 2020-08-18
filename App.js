102ccee3e71fb812c134b56ffc37aea70c36d881
fabianzielinski <fabian.zielinski@o2.pl>
2020-08-18 23:10:57 +0200

    Random click work ok

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
+const fotosUrl = [
+  "./home-office-336373_1920.jpg",
+  "./light-bulb-1246043_1920.jpg",
+  "./milky-way-984050_1920.jpg",
+  "./ocean-918999_1920.jpg",
+];
+
+let fotosGalery = [
+  { id: 0, number: 0 },
+  { id: 1, number: 1 },
+  { id: 2, number: 2 },
+  { id: 3, number: 3 },
+  { id: 4, number: 0 },
+  { id: 5, number: 0 },
+  { id: 6, number: 3 },
+  { id: 7, number: 1 },
+  { id: 8, number: 2 },
+];
+
 class Foto extends React.Component {
   sendIdFoto = (e) => {
     let index = e.target.getAttribute("id");
-    console.log(e.target);
-    console.log(index);
+    // console.log(e.target);
+    // console.log(index);
     this.props.changeFoto(index);
   };
 
@@ -17,23 +36,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      fotosUrl: [
-        "./home-office-336373_1920.jpg",
-        "./light-bulb-1246043_1920.jpg",
-        "./milky-way-984050_1920.jpg",
-        "./ocean-918999_1920.jpg",
-      ],
-      fotosGalery: [0, 1, 2, 3, 1, 0],
-      id: 0,
+      flag: true,
     };
   }
 
-  makeId = () => {
-    let Id = this.state.id;
-    let newId = this.state.id++;
-    // this.setState({ id: newId });
-    return Id, newId;
-  };
+  // makeId = () => {
+  //   id = id + 1;
+  //   return id;
+  // };
 
   random = (range) => {
     const randomNamber = Math.floor(Math.random() * (range - 0));
@@ -41,41 +51,23 @@ class App extends React.Component {
   };
 
   changeFoto = (nrIdFoto) => {
-    console.log(nrIdFoto);
-    let a = this.state.fotosGalery.slice(); //creates the clone of the state
-    console.log(a);
-    a[nrIdFoto] = this.random(4);
-    console.log(a[nrIdFoto]);
-    this.uppdateState(a);
-    // this.setState({ fotosGalery: a });
+    // console.log(nrIdFoto);
+    let a = fotosGalery.slice(); //creates the clone of the state
+    // console.log(a);
+    a[nrIdFoto].number = this.random(4);
+    // console.log(a[nrIdFoto]);
+    fotosGalery = a;
+    this.setState({ flag: !this.state.flag });
     return a;
   };
-
-  uppdateState = (a) => {
-    console.log(a);
-    this.setState({
-      fotosGalery: a,
-    });
-    console.log(this.state.fotosGalery);
-  };
-
-  // componentWillReceiveProps(nextProps) {
-  //   this.setState((a, newId) => {
-  //     console.log(a);
-  //     return {
-  //       fotosGalery: nextProps.a,
-  //       id: nextProps.newId,
-  //     };
-  //   });
-  // }
 
   render() {
     return (
       <div>
-        {this.state.fotosGalery.map((item) => (
+        {fotosGalery.map((item) => (
           <Foto
-            id={/*this.state.fotosGalery.indexOf(item)*/ this.makeId()}
-            url={this.state.fotosUrl[item]}
+            id={item.id}
+            url={fotosUrl[item.number]}
             changeFoto={this.changeFoto}
           />
         ))}
